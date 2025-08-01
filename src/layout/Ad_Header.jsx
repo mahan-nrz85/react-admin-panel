06d2f6a838177038fa9092fd7fce98039236e185
mahan <mahan85norouzi@gmail.com>
2025-08-01 23:45:17 +0330

    update header & profile

--- src/layout/Ad_Header.jsx
+++ src/layout/Ad_Header.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router'
+import {Settings , Home , Person} from '@mui/icons-material';
 import styled from 'styled-components'
+import {AnimatePresence , motion} from 'motion/react'
 const Styles = styled.div`
     display: flex;
     justify-content: space-between;
@@ -10,14 +13,90 @@ const Styles = styled.div`
     height: 100%;
     background-color: var(--bg-sec);
     .h-container {
-        width: 75%;
+        width: 100%;
     }
 `
+
+
 function Ad_Header() {
+  const [profile , setProfile] = useState(null)
+  const [open , setOpen] = useState(false)
+  useEffect(() => {
+    const url = (localStorage.getItem("profile_url")) ? JSON.parse(localStorage.getItem("profile_url")) : null;
+    if(url){
+      setProfile(url)
+      return;
+    }
+  })
+  const handleMenu = () => {
+    setOpen(prev => !prev)
+  }
   return (
     <Styles>
-        <div className='h-container'>
+        <div className='h-container flex justify-between items-center'>
+          <div>
             <span className='text-2xl '>پنل ادمین</span> <span>- خوش آمدید</span>
+          </div>
+            <div className='relative'>
+              <img 
+                  src={`${(profile !== null) ? profile : '/image/profile.png'}`}
+                  className='w-10 h-10 rounded cursor-pointer'
+                  onClick={handleMenu}
+              />
+              <AnimatePresence>
+                {
+                  open &&
+                  <motion.div
+                    initial={{ y: 10, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    exit={{ y: -10, opacity: 0 }}
+                    transition={{ duration: 0.2 }}
+                    className='absolute left-0 w-40 z-50 top-[3.2rem]'
+                  >
+                    <div className='absolute bg-white  w-full  rounded text-black !p-3'>
+                        <p>ماهان نوروزی</p>
+                        <p className='text-[14px] text-gray-500'>مدیر</p>
+                        <div className='w-full bg-gray-400 h-[1px]'></div>
+                        <ul className='!mt-2 flex flex-col gap-3'>
+                          <Link to={'/setting/profile'}>
+                            <li className='flex items-center gap-1'>
+                              <Settings 
+                                sx={{
+                                  width : '20px',
+                                  height : '20px'
+                                }}
+                              />
+                              <p className='text-[14px]'>پروفایل</p>
+                            </li>
+                          </Link>
+                          <Link to={'/dashboard'}>
+                            <li className='flex items-center gap-1'>
+                              <Home 
+                                sx={{
+                                  width : '20px',
+                                  height : '20px'
+                                }}
+                              />
+                              <p className='text-[14px]'>داشبورد</p>
+                            </li>
+                          </Link>
+                          <Link>
+                            <li className='flex items-center gap-1'>
+                              <Person 
+                                sx={{
+                                  width : '20px',
+                                  height : '20px'
+                                }}
+                              />
+                              <p className='text-[14px]'>حساب کاربری</p>
+                            </li>
+                          </Link>
+                        </ul>
+                    </div>
+                  </motion.div>
+                }
+              </AnimatePresence>
+            </div>
         </div>
     </Styles>
   )
--- src/pages/settingPages/Ad_profile.jsx
+++ src/pages/settingPages/Ad_profile.jsx
@@ -9,25 +9,41 @@ function Ad_profile() {
   const [previewFile , setPreviewFile] = useState(null)
   const [file , setFile] = useState(null)
   const [image , setImage] = useState('')
+  const [profile , setProfile] = useState('')
   const [loading , setLoading] = useState(false);
   const [open , setOpen] = useState(false)
   const [dialogNum , setDialogNum] = useState(1)
- useEffect(() => {
-  const img = JSON.parse(localStorage.getItem("img_url") || '""');
-  if(img === undefined){
-    localStorage.removeItem("img_url");
-    return;
-  } else {
-    setImage(img);
-  }
-}, []);
+  useEffect(() => {
+    const img = JSON.parse(localStorage.getItem("img_url") || '""');
+    const profile = JSON.parse(localStorage.getItem("profile_url") || '""');
+    if(img === undefined && profile === undefined){
+      localStorage.removeItem("img_url");
+      localStorage.removeItem("profile_url");
+      return;
+    } else {
+      setImage(img);
+      setProfile(profile);
+    }
+  }, []);
 
-  
+  const recentactivities = [
+    {
+      title : 'آپدیت کاربر های سایت',
+
+    },
+    {
+      title : 'بن کردن بعضی از کاربران'
+    },
+    {
+      title : "به روزرسانی گزارش های سایت"
+    }
+  ]
   const handleModal = () => {
     setOpen(prev => !prev)
     setPreviewFile(null)
     setFile(null)    
   }
+  // show prew image in front
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if(!file){
@@ -37,6 +53,7 @@ function Ad_profile() {
     setPreviewFile(URL.createObjectURL(file));
     setFile(file)
   }
+  // send img to server for upload and save in local storage for show and update ui
   const uploadImage = async () => {
     if(!file){
       enqueueSnackbar("فایلی انتخاب نشده است" , {variant: "error"})
@@ -66,15 +83,55 @@ function Ad_profile() {
         handleModal()
       }
     }
+    
+  }
+  // send profile to server for upload and save in local storage for show and update ui
+  const uploadProfileImage = async () => {
+    if(!file){
+      enqueueSnackbar("فایلی انتخاب نشده است" , {variant: "error"})
+      return;
+    } 
+    try {
+      setLoading(true)
+      const sendReq = await CloudinaryUploader(file)
+      setLoading(false)
+      if(sendReq.error){
+        let newErr = sendReq.error.message.slice(0,19)
+        if(newErr === "File size too large"){
+          enqueueSnackbar("فایل باید کمتر از 10MB باشد" , {variant:"error"})
+        }        
+      }
+      localStorage.setItem("profile_url" , JSON.stringify(sendReq.secure_url))  
+      setProfile(sendReq.secure_url)    
+      handleModal()
+      setFile(null)
+      
+    } catch (error) {
+      if(error){
+        enqueueSnackbar("مشکلی در اینترنت یا سرور پیش آمده است" , {variant:"error"});
+        setLoading(false)
+        setPreviewFile(null)
+        setProfile(null)
+        handleModal()
+      }
+    }
       
     
   }
-  console.log(image);
-  
+  const renderActivities = recentactivities.map((item , i ) =>{
+    return (
+      <div 
+        key={i}
+        className='!p-2 text-[14px] hover:bg-blue-700 hover:rounded'
+      >
+        <h3>{item.title}</h3>
+      </div>
+    )
+  })
   return (
     <>
     <SnackbarProvider>
-      <div className='main-container flex items-center justify-center w-full'>
+      <div className='main-container flex flex-col items-center justify-center w-full'>
           <div className='main-div bg-[#1A1D1F] w-[80%] rounded-2xl relative h-[300px]'>
               <div
                 className=' bg-blue-400 rounded-2xl !p-3 mx-auto w-[100%] h-[180px] shadow '
@@ -85,7 +142,7 @@ function Ad_profile() {
                   backgroundSize : 'cover'
                 }} 
                 >
-                <button 
+                <button
                   type='button' 
                   className='  bg-white/40 !p-2 cursor-pointer absolute left-[1rem] top-[8rem] rounded-2xl z-30'
                   onClick={() => {
@@ -100,7 +157,7 @@ function Ad_profile() {
               </div>
               <div className='flex gap-5 items-end justify-between absolute bottom-10 !p-3 w-full'>
                 <div className='flex items-end gap-5'>
-                  <img src={'/image/profile.png'} alt='پروفایل' className='w-25 rounded-[9999px]'/>
+                  <img src={`${(profile) ? profile : '/image/profile.png' }`} alt='پروفایل' className='w-25 h-25 rounded-[9999px]'/>
                     <div className='content'>
                       <h2 className='text-2xl'>ماهان نوروزی</h2>
                       <p>نقش : مدیر</p>
@@ -114,9 +171,25 @@ function Ad_profile() {
                       handleModal()
                     }}
                   >
-                    افزودن عکس
+                    {(profile) ? 'ویرایش عکس' : 'افزودن عکس'}
                   </button>
               </div>
+          </div>
+          {/* content */}
+          <div className='content !mt-5 flex  gap-5 w-[80%]'>
+            <div className='bg-blue-400/30 w-[70%] !p-3 rounded'>
+              d
+            </div>
+            <div className="relative before:content-[''] before:absolute before:w-3 before:h-6 before:rounded before:right-1 before:bg-blue-500 bg-blue-400/30 w-[30%] !p-5  rounded">
+              <p className='border-b-1 !p-1 border-gray-500'>
+              فعالیت های اخیر
+              </p>
+            <div className="!mt-2">
+                {
+                  renderActivities
+                }
+            </div>
+            </div>
           </div>
       </div>
 
@@ -188,7 +261,7 @@ function Ad_profile() {
             )
             : (
                 <div className='flex flex-col items-center'>
-                <div className='w-30 h-30 rounded-[9999px] relative flex items-center justify-center' style={{
+                <div className='w-32 h-30 rounded-[9999px] relative flex items-center justify-center' style={{
                   border : '4px dotted gray'
                 }}>
                   {
@@ -209,7 +282,7 @@ function Ad_profile() {
                       )
                       :
                         (
-                          <img src={previewFile} className='w-fit h-[250px]'/>
+                          <img src={previewFile} className='w-30 h-30 rounded-[9999px]'/>
                         )
                   }
 
@@ -222,7 +295,7 @@ function Ad_profile() {
                       (
                       <button 
                         type='button'
-                        onClick={uploadImage}
+                        onClick={uploadProfileImage}
                         className='bg-[var(--btn-bg)]  !p-3 w-[70px] rounded-2xl !mt-4 cursor-pointer text-white transition-transform duration-150 focus:scale-[0.9] active:scale-[0.9]'
                         style={{
                         padding: '1rem',
